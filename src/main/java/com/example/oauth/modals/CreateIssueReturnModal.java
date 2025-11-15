ea136d333c9bdbf96bf4b650ccbe9fcf415c6103
Ankit Ranjan <62781709+ankitranjan5@users.noreply.github.com>
2025-11-16 00:23:19 +0530

    Implemented Jira OAuth

---
+++ src/main/java/com/example/oauth/modals/CreateIssueReturnModal.java
@@ -0,0 +1,31 @@
+package com.example.oauth.modals;
+
+public class CreateIssueReturnModal {
+
+    String id;
+    String key;
+    String self;
+    public CreateIssueReturnModal() {
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getKey() {
+        return key;
+    }
+    public void setKey(String key) {
+        this.key = key;
+    }
+    public String getSelf() {
+        return self;
+    }
+    public void setSelf(String self) {
+        this.self = self;
+    }
+}
